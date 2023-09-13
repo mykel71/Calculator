40166fe8fed6c4b150addc421dc23023ab224a3e
Michael Shepherd M <shepherd.munemo@outlook.com>
2023-09-13 10:10:59 +0200

    Initial commit

---
+++ Calculator/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Domain;
+
+namespace CalculatorTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var calculator = new Calculator();
+
+            if (calculator.Sum(2, 2) != 4)
+                throw new Exception();
+        }
+    }
+}
